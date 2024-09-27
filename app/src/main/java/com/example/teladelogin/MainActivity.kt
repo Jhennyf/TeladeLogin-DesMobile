7e2b663b651dceaa91f8117e394e4163dd55cd87
jhennyf <jhennyfestudos2021@gmail.com>
2024-09-27 17:27:19 -0300

    :sparkles: feat: Página de Login

--- app/src/main/java/com/example/teladelogin/MainActivity.kt
+++ app/src/main/java/com/example/teladelogin/MainActivity.kt
@@ -4,29 +4,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.teladelogin.R.id.btnlogin
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var textView: TextView
+
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        button = findViewById(btnlogin)
+
+        button = findViewById(R.id.btnLogin)
+        textView = findViewById(R.id.txtMessagem)
+
+        button.setOnClickListener {
+            onClickMensagem(it)
+        }
     }
-    public fun onclickMensagem(view: View){
-        var textView: TextView = findViewById(R.id.txtMsg)
-        textView = findViewById(R.id.txtMsg)
+
+    private fun onClickMensagem(view: View) {
+        textView.visibility = View.VISIBLE
     }
 }
